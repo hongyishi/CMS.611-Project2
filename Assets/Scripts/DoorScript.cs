65af7cc307089179b29857ec6c70f26395cf4dd6
druckgreen <druckgreen@gmail.com>
2017-02-27 01:23:34 -0500

    Door script

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorScript : MonoBehaviour {
+
+    public GameObject Kid;
+    public GameObject Monster;
+    public Text winText;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Monster"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        else if (other.gameObject.CompareTag("Kid"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        if (!Kid.activeSelf && !Monster.activeSelf)
+        {
+
+        }
+    }
+}
